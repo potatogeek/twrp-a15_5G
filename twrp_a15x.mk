4fb2614eb790ab5a07d7093c57896a1998e38ed5
potatogeek <gnogueira3164@gmail.com>
2025-06-01 13:55:13 -0300

    fix omni

--- twrp_a15x.mk
+++ twrp_a15x.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from a15x device
 $(call inherit-product, device/samsung/a15x/device.mk)
 
 PRODUCT_DEVICE := a15x
-PRODUCT_NAME := omni_a15x
+PRODUCT_NAME := twrp_a15x
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A156M
 PRODUCT_MANUFACTURER := samsung
